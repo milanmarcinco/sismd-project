e22ec14df8e686d7ce8db16e35fdb9ec4a4836a0
Milan Marcinčo <milan@marcinco.xyz>
2026-03-25 19:01:14 +0000

    Init commit - base structure, starter code

---
+++ src/_01_Sequential.java
@@ -0,0 +1,7 @@
+public class _01_Sequential {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+        System.out.println("This is a sequential program");
+    }
+}
